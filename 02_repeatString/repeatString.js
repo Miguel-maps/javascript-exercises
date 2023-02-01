2c94e733a3ebf81f8f99080f52324b56e45604b2
Miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-01 21:05:50 +0100

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    if (num >= 0) {
+        let aux = "";
+        for (let i = 0; i < num; i++) {
+            aux += string;
+        }
+        return aux;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
