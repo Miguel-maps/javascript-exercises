9bb0a087a7f53c0e8f41373233d6150cb2a1e5fc
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-02 11:40:23 +0100

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+function reverseString(string){
+    return string.split("").reverse().join("");
 };
+
+console.log(reverseString("string"));
 
 // Do not edit below this line
 module.exports = reverseString;
