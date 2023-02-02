0e5ec677a066b12ce33c5d008448d247a1e66529
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-02 13:12:07 +0100

    Solve exercises 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+function removeFromArray(array, ...args) {
+    return array.filter((item) => !args.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+function sumAll(a, b) {
+
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return 'ERROR';
+    }
+ 
+    let smallest = (a < b) ? a : b;
+    let biggest = (a > b) ? a : b;
+    let sum = smallest;
+    for (let i = smallest; i <= biggest; i++) {
+        sum = sum + i;
+    }
+    return sum - 1;
 
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
+
